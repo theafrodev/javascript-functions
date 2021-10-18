1fd0408e20f96cf002995ea49a1441da939d3a6a
theafrodev <fialorchristian@gmail.com>
2021-10-18 18:20:17 +0000

    getNeighboursof - nostrings

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -114,11 +114,12 @@ const printCells = (state) => {
 
 };
 
-const getNeighborsOf = ([x, y]) => {
-  return(
-      `[${x-1},${y+1}] [${x},${y+1}] [${x+1},${y+1}]\n[${x-1},${y}] [${x},${y}] [${x+1},${y}]\n[${x-1},${y-1}] [${x},${y-1}] [${x+1},${y-1}]`
-  );
-};
+const getNeighborsOf = ([x, y]) => { [
+  [x-1, y+1] [x, y+1] [x+1, y+1],
+  [x-1, y] [x, y] [x+1, y],
+  [x-1, y-1] [x, y-1] [x+1, y-1]
+]
+}
 
 const getLivingNeighbors = (cell, state) => {};
 
