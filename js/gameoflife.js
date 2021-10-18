ccf10a0312d5f09242b294b4c139046411b6ad7f
theafrodev <fialorchristian@gmail.com>
2021-10-18 18:27:11 +0000

    getNeighboursOf - return array

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -114,12 +114,11 @@ const printCells = (state) => {
 
 };
 
-const getNeighborsOf = ([x, y]) => { [
-  [x-1, y+1] [x, y+1] [x+1, y+1],
-  [x-1, y] [x, y] [x+1, y],
-  [x-1, y-1] [x, y-1] [x+1, y-1]
+const getNeighborsOf = ([x, y]) => [
+  [x-1, y+1], [x, y+1], [x+1, y+1],
+  [x-1, y], [x, y], [x+1, y],
+  [x-1, y-1], [x, y-1], [x+1, y-1]
 ]
-}
 
 const getLivingNeighbors = (cell, state) => {};
 
