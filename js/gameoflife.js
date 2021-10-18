6ab9aa496950c4e543ed6345776fe05125af3cdf
theafrodev <fialorchristian@gmail.com>
2021-10-18 18:28:24 +0000

    getneighboursof - remove current

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -116,7 +116,7 @@ const printCells = (state) => {
 
 const getNeighborsOf = ([x, y]) => [
   [x-1, y+1], [x, y+1], [x+1, y+1],
-  [x-1, y], [x, y], [x+1, y],
+  [x-1, y],             [x+1, y],
   [x-1, y-1], [x, y-1], [x+1, y-1]
 ]
 
